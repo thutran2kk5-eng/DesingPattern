9bf849977ff6bac88d64ff7dd4268abf1c6c3ec0
Thu tran <thutran2kk5@gmail.com>
2026-03-05 16:44:09 +0700

    delete mtk

--- mtk/src/mtk/ChocolateCandy.java
+++
@@ -1,9 +0,0 @@
-package mtk;
-
-public class ChocolateCandy extends Candy {
-
-    @Override
-    public void produce() {
-        System.out.println("Producing Chocolate Candy");
-    }
-}
--- mtk/src/mtk/MintCandy.java
+++
@@ -1,9 +0,0 @@
-package mtk;
-
-public class MintCandy extends Candy {
-
-    @Override
-    public void produce() {
-        System.out.println("Producing Mint Candy");
-    }
-}
